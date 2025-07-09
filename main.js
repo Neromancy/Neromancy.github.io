1be2a436e9bd9695a95e59b3cd6dbae59e6e3b07
Neromancy <andrewhermawan1705@gmail.com>
2025-07-09 11:03:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 document.getElementById('clickBtn').addEventListener('click', () => {
   alert('Testing PWA berhasil');
 });
+
+const animal = document.getElementById('gerak');
+let pos = 0;
+let speed = 2;
+
+function moveAnimal() {
+  pos += speed;
+  animal.style.left = pos + 'px';
+
+  if (pos > window.innerWidth) {
+    pos = -animal.width;
+  }
+
+  requestAnimationFrame(moveAnimal);
+}
+
+moveAnimal();
